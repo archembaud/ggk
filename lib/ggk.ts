f627a46db39ae076f2627ab3ec645be10d65f7c9
Matt Smith <archembaud@hotmail.com>
2025-06-19 10:06:52 +0800

    First draft for SDK

--- lib/ggk.ts
+++ lib/ggk.ts
@@ -1,11 +1,10 @@
-#!/usr/bin/env node
 import * as cdk from 'aws-cdk-lib';
 import { GgkStack } from './ggk-stack';
 
 const app = new cdk.App();
 new GgkStack(app, 'GgkStack', {
-  env: { 
-    account: process.env.CDK_DEFAULT_ACCOUNT, 
-    region: process.env.CDK_DEFAULT_REGION 
+  env: {
+    account: process.env.CDK_DEFAULT_ACCOUNT,
+    region: process.env.CDK_DEFAULT_REGION
   },
-}); 
+});
